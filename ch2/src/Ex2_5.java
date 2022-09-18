ce9306a203eb0a53de22a5d2d9982f2200abe4cd
kimbro97 <rlagudwog@gmail.com>
2022-09-18 15:06:53 +0900

    init

---
+++ ch2/src/Ex2_5.java
@@ -0,0 +1,11 @@
+public class Ex2_5 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 5;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
---
+++ ch2/src/Ex2_6.java
@@ -0,0 +1,14 @@
+public class Ex2_6 {
+    public static void main(String[] args) {
+        int x = 100; // 정수
+        double pi = 3.14; // 실수
+        char ch = 'a'; // 문자 하나
+        String str = "abc"; // 문자열
+
+        System.out.println(x);
+        System.out.println(pi);
+        System.out.println(ch);
+        System.out.println(str);
+
+    }
+}
